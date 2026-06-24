e5947c553bd48969a1b2d393743ee60946981533
Tere Marroquín <1725110143@utectulancingo.edu.mx>
2026-06-24 14:02:23 +0000

    CREATED chat00

---
+++ demos/chat00/app.py
@@ -0,0 +1,12 @@
+from ollama import chat
+
+response = chat(
+    model='llama3.2:3b',
+    messages=[
+        {
+            'role': 'user',
+             'content': 'Hola!'
+        }
+        ],
+)
+print(response.message.content)
